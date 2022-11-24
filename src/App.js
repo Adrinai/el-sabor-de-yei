24e6450a94be75c08aef5233b8f950765d85b48c
Adrinai <adrinai.conchi1@yahoo.es>
2022-11-25 00:01:18 +0100

    pruebas con chakra

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbSeparator,
+} from '@chakra-ui/react';
+import { Button, ButtonGroup } from '@chakra-ui/react';
+import { EmailIcon, PhoneIcon, AddIcon, WarningIcon } from '@chakra-ui/icons'
+import { Box, Flex } from "@chakra-ui/react"
 import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header>
-        <nav>
-          <a>contacto</a>
-          <a></a>
-        </nav>
+        <Box display = "flex" alignContent="right" justifyContent="space-between" bg="brand.accent">
+        
+        <Button colorScheme='teal' variant='outline'>
+            contacto
+        </Button>
+        
+        <Breadcrumb>
+  <BreadcrumbItem>
+    <BreadcrumbLink href='#'><EmailIcon></EmailIcon></BreadcrumbLink>
+  </BreadcrumbItem>
+
+</Breadcrumb>
+<Flex align="center" justify="center">
+  
+</Flex>
+        </Box>
+      <Box>
+      <Breadcrumb>
+  <BreadcrumbItem>
+    <BreadcrumbLink href='#'>contacto</BreadcrumbLink>
+  </BreadcrumbItem>
+
+  <BreadcrumbItem>
+    <BreadcrumbLink href='#'>sobre nosotros</BreadcrumbLink>
+  </BreadcrumbItem>
+
+  <BreadcrumbItem isCurrentPage>
+    <BreadcrumbLink href='#'>productos</BreadcrumbLink>
+  </BreadcrumbItem>
+  
+</Breadcrumb>
+        </Box>
       </header>
     </div>
   );
---
+++ src/resorces/theme/index.js
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+
+  brand: {
+    primary: "#AA7BC3",
+    secundary: "#CC92C2",
+    accent: "#DBA8AC",
+  },
+}
+const Button={
+    border:{
+        
+    }
+
+}
+export const theme = extendTheme ({colors})

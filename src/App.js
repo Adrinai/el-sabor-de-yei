0e7b15175f7615da62096505365a84a6043a181f
Lulubonucci <bonucci64@gmail.com>
2022-11-30 18:50:05 +0100

    agrego ruta de contacto

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Inicio from "./components/Inicio/Inicio";
 import QuienesSomos from "./components/QuienesSomos";
 import Productos from "./components/Productos";
+import Contacto from "./components/Contacto";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Inicio />} />
         <Route path="/quienesSomos" element={<QuienesSomos />} />
         <Route path="/productos" element={<Productos />} />
+        <Route path="/contacto" element={<Contacto />} />
       </Routes>
     </BrowserRouter>
   );

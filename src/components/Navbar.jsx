5bde9eefaf35ce59e3606062c868e0eb2fa5bac5
Lulubonucci <bonucci64@gmail.com>
2022-11-30 19:28:29 +0100

    link a inicio, y quienesSomos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,42 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Container,
+  Wrapper,
+  Menu,
+  MenuItem,
+  MenuItemLink,
+} from "./NavbarStyle";
 
-import React from 'react'
-import { Container, Wrapper, Menu, MenuItem, MenuItemLink,} from './NavbarStyle'
-
-import { IconContext } from 'react-icons';
+import { IconContext } from "react-icons";
 
 const Navbar = () => {
   return (
     <Container>
-       <Wrapper>
-       <IconContext.Provider value={{style: {fontSize: "1rem"}}}>
-
-                <Menu>
-                    <MenuItem>
-                        <MenuItemLink>
-                            INICIO
-                        </MenuItemLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuItemLink>
-                            QUIENES SOMOS
-                        </MenuItemLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuItemLink>
-                            PRODUCTOS
-                        </MenuItemLink>
-                    </MenuItem>
-                    <MenuItem>
-                        <MenuItemLink>
-                            CONTACTO
-                        </MenuItemLink>
-                    
-                        </MenuItem>
-                            </Menu>
-                        </IconContext.Provider>
-       </Wrapper>
+      <Wrapper>
+        <IconContext.Provider value={{ style: { fontSize: "1rem" } }}>
+          <Menu>
+            <MenuItem>
+              <MenuItemLink>
+                <Link to="/">Inicio</Link>
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink>
+                <Link to="/quienesSomos">Quienes Somos</Link>
+              </MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink>PRODUCTOS</MenuItemLink>
+            </MenuItem>
+            <MenuItem>
+              <MenuItemLink>CONTACTO</MenuItemLink>
+            </MenuItem>
+          </Menu>
+        </IconContext.Provider>
+      </Wrapper>
     </Container>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

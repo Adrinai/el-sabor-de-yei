31af878c2433d84fb039ba8b0d0b868a336a94e2
Adrinai <adrinai.conchi1@yahoo.es>
2022-12-01 00:01:38 +0100

    continuando con fondo pantalla

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Logo from "../Logo";
+import Main from "../Main";
 import Footer from "../Footer";
 
 export default function Inicio() {
   return (
     <div>
+      
       <Navbar />
       <Logo />
+    
+      <Main/>
       <Footer className="footer"></Footer>
     </div>
   );
---
+++ src/components/MainStyle.js
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+export const Main = styled.div`
+        width: 100%;
+        height: 100%;
+//     background-image: url('../src/assets/img/portada2.jepg');
+//     background-repeat: no-repeat;
+//     background-size: cover;
+//     height: 100%;
+//     position: relative;
+//     padding-bottom: 3em ;
+//   }
+  
+  <Stack direction='row'>
+  <Image
+    boxSize='100%'
+    objectFit='cover'
+    background-image: url('../src/assets/img/tartafloresverdes.jpeg');
+    alt='Dan Abramov'
+  />
+  <Image
+    boxSize='150px'
+    objectFit='cover'
+    src='https://bit.ly/dan-abramov'
+    alt='Dan Abramov'
+  />
+  <Image boxSize='200px' src='https://bit.ly/dan-abramov' alt='Dan Abramov' />
+</Stack>
+  `

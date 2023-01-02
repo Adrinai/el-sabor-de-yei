ccf637b7c26f6a34f91343bc6b96cf4b6f69f399
Adrinai <adrinai.conchi1@yahoo.es>
2023-01-02 23:48:01 +0100

    informacion contacto-tarjeta contacto-redes sociles-telefonon

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -12,48 +12,28 @@ export default function Contacto() {
     
     
   return (
-    // <div className="portada">
-    //   <div>
-    //     <Navbar />
-    //     <Logo />
-    //   </div>
-    //   <div className='formulario'>
-    //     <Formulario/>
-    //   </div>
-    //   <div>
-    //    <InfoContacto/>
-    //   </div>
-    //   <div className="footer">
-    //     <Footer />
-    //   </div>
-    // </div>
-    <Wrap className="portada" >
-  <WrapItem>
-    {/* <Center w='180px' h='80px' bg='red.200'>
-      Box 1
-    </Center>  */}
     
+    <div className="portada">
       <div>
-       <Navbar />
-       
+      <div>
+        <Navbar />
         <Logo />
       </div>
-  </WrapItem>
-  <WrapItem>
-    {/* <Center w='180px' h='80px' bg='green.200'>
-      Box 2
-    </Center> */}
-  </WrapItem>
-  <WrapItem>
-    {/* <Center w='180px' h='80px' bg='tomato'>
-      Box 3
-    </Center> */}
-  </WrapItem>
-  <WrapItem>
-    {/* <Center w='180px' h='80px' bg='blue.200'>
-      Box 4
-    </Center> */}
-  </WrapItem>
-</Wrap>
+      <div className='contenedor'>
+      <div className='formulario'>
+        <Formulario/>
+      </div>
+      
+      <div className='info'>
+        <div className='formulario'>
+       <InfoContacto/>
+      </div>
+      </div>
+      </div>
+    </div>
+      <div className="footer">
+        <Footer />
+      </div>
+    </div> 
   )
   }
--- src/components/InfoContacto.jsx
+++ src/components/InfoContacto.jsx
@@ -1,34 +1,49 @@
 import React from 'react'
 import "../components/InfoContactoStyle.css";
 import Logo from './Logo';
-import { Flex, Heading, Spacer, SocialButton } from '@chakra-ui/react'
-import {
-  Box,
-  chakra,
-  Container,
-  Link,
-  Stack,
-  Text,
-  useColorModeValue,
-  VisuallyHidden,
-  
-
-} from '@chakra-ui/react';
-import { FaInstagram, FaFacebook, FaRegEnvelope } from 'react-icons/fa';
+import { FaInstagram, FaFacebook, FaRegEnvelope, FaWhatsapp} from "react-icons/fa";
+import { AiFillPhone } from "react-icons/ai";
 
 const InfoContacto = () => {
   return (
-    
+      
     <div className='info'>
-    <div className="logo">
-      <div>
+    <div>
+      <div className='ico'>
         <Logo />
       </div>
-      <div className='text'>
-        
+      
+        <div className='titulo'>
       <p>
       Nos interesa tu opinión, si tienes dudas o sugerencias ¡escríbenos!
       </p>
+      </div>
+      <div className='text'>
+      <div>
+      <FaFacebook className='enlaces'/>
+      </div>
+      <div >
+      <a className='enlaces'href={'https://www.facebook.com/elsabordeyei'}>elsabordeyei</a>
+      </div>
+      <div>
+      <FaInstagram className='enlaces'/>
+      </div>
+      <div>
+      <a className='enlaces'href={'https://bit.ly/3fVS0kv'}>elsabordeyei</a>
+      </div>
+      <div >
+    <FaRegEnvelope className='enlaces'/> 
+      </div>
+      <div>
+      <a className='enlaces'href={'mailto:grupoelsabordeyei0711ca@gmail.com?Subject=Más%20información%20sobre%20sus%20productos'}>grupoelsabordeyei0711ca@gmail.com</a>
+      </div>
+      <div >
+      <AiFillPhone className='enlaces'/> 
+      <div> 
+      <FaWhatsapp className='enlaces'/><a href={'https://walink.co/eea979'}> +58 424-2924205</a>
+    </div>
+      </div>
+      
       </div>
       </div>
       </div>

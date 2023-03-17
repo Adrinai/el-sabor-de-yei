a0c9c437afe104282d00cb5382d46e957ea06d94
Adrinai <adrinai.conchi1@yahoo.es>
2023-03-17 18:52:54 +0100

    creando más componentes

--- src/components/pages/Productos.jsx
+++ src/components/pages/Productos.jsx
@@ -1,25 +1,71 @@
-import React from 'react';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './productos.css';
 import Navbar from "../navbar/Navbar";
-import Carrusel from "../carrusel/CarruselCumple"
-import Footer from "../footer/Footer";
 import Logo from "../logo/Logo";
+import Footer from "../footer/Footer";
+import perfil from "../../assets/img/perfil 4.jpg";
+import "../pages/MainStyle.scss";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+ 
 
+function Productos() {
+  const productos = [
+    {
+      id: 1,
+      nombre: "Pastel de zanahoria",
+      imagen: "/img/pastel-zanahoria.jpg",
+      descripcion: "Un delicioso pastel de zanahoria con nueces.",
+      precio: 15.99,
+    },
+    {
+      id: 2,
+      nombre: "Empanadas de carne",
+      imagen: "/img/empanadas-carne.jpg",
+      descripcion: "Unas ricas empanadas de carne especiadas.",
+      precio: 2.99,
+    },
+    {
+      id: 3,
+      nombre: "Arepa de chicharrón",
+      imagen: "/img/arepa-chicharron.jpg",
+      descripcion: "Una arepa rellena de chicharrón y queso.",
+      precio: 7.99,
+    },
+  ];
 
-export default function Productos() {
   return (
-    <div className='phater'>
-      <div className="cabecera">
-        <Logo />
-       <Navbar />
-    </div>
-      <div className="wrapper">
-        <div className='card mt-5 mx-auto w-25 shadow-lg'>
-            <Carrusel />
-        </div>
+    <Container fluid>
+      <Row>
+        <Col>
+          <Logo/><Navbar/>
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+    <div>
+      <h1>Productos</h1>
+      <div className="row">
+        {productos.map((producto) => (
+          <div className="col-md-4" key={producto.id}>
+            <div className="card">
+              <img className="card-img-top" src={producto.imagen} alt={producto.nombre} />
+              <div className="card-body">
+                <h5 className="card-title">{producto.nombre}</h5>
+                <p className="card-text">{producto.descripcion}</p>
+                <p className="card-text">${producto.precio}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
-      <div className="footer">
-        <Footer />
-      </div>
     </div>
+    </Row> 
+   </Container>
   );
 }
+
+export default Productos;
--- src/components/pages/QuienesSomos.jsx
+++ src/components/pages/QuienesSomos.jsx
@@ -4,58 +4,39 @@ import Logo from "../logo/Logo";
 import Footer from "../footer/Footer";
 import perfil from "../../assets/img/perfil 4.jpg";
 import "../pages/MainStyle.scss";
-import Card from 'react-bootstrap/Card';
-import { CardImg, CardTitle, CardText, CardBody } from 'reactstrap';
-
-export default function QuienesSomos() {
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+ 
+function QuienesSomos() {
   return (
-  
-      
-<div className="">
-      <div className="cabecera">
-        <div>
-          <Logo />
-        </div>
-        <div>
-        <Navbar />
-        </div>
-      </div>
-<div className='container p-0 '>
-    <div className='row bg-dark'></div>
-
-<Card className=" Image-fluid my-1">
-<CardImg
-  alt="Card image cap"
-  src={perfil}
-  style={{
-    height: "148px"
-  }}
-  top
-  width="200px"
-/>
-<CardBody>
-  <CardTitle tag="h5">
-    EL SABOR DE YEI
-  </CardTitle>
-  <CardText>
-  El sabor de yei conocido en instagram como @el sabor.ve , es una empresa familiar que se encarga de elaborar tortas temáticas panes dulces pasapalos, Brownies, tortas 3D en fondant y mucho más.
+    <Container fluid>
+      <Row>
+        <Col>
+          <Logo/><Navbar/>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Image src={perfil} fluid />
+          <p>El sabor de yei conocido en instagram como @el sabor.ve , es una empresa familiar que se encarga de elaborar tortas temáticas panes dulces pasapalos, Brownies, tortas 3D en fondant y mucho más.
               Fue fundada en julio del 2018 pero es estando en  Pandemia dónde se  dio a conocer mucho más e incluso internacionalmente, ya que familiares que viven fuera de Venezuela nos contactan para mandar hacer un agasajo a sus seres queridos que están aún en nuestro país.   Mireya Viana , Nathalia Jalón y Gabriel jalón  son las personas que trabajan en el sabor de yei 
               El origen del nombre se debe a que es como suelen llamar a Mireya cuando era pequeña, así que ella decidió incluirlo en el  emprendimiento.
                Está ubicado en Venezuela exactamente en Cua , estado Miranda en su propio hogar.
               Mireya y Nathalia poseen conocimientos de repostería panadería ventas y finanzas por lo que esta idea le genera un negocio . 
-              Gabriel se encarga del delivery y deL amasado de las pizzas y panes .
-  </CardText>
-  
-</CardBody>
-</Card>
-
-
- </div>
-<div className="footer">
-          <Footer />
-        </div>
+              Gabriel se encarga del delivery y deL amasado de las pizzas y panes .</p>
+        </Col>
+      </Row>
       
- 
-</div>
-   );
-  }  
+      <Row>
+        <Col>
+          <Footer/>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default QuienesSomos;
+
